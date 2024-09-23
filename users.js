d39beffa81592849d3ac29cba1a090a96a7871bd
Nedel Ayón <119768960+NedelAyon-jv@users.noreply.github.com>
2024-09-23 11:57:17 -0700

    Add files via upload

---
+++ users.js
@@ -0,0 +1,17 @@
+[
+    {
+        "email": "Nedel@example.com",
+        "password": "123456",
+        "name": "Nedel Ayon"
+    },
+    {
+        "email": "Vecs@example.com",
+        "password": "1234567",
+        "name": "Victor Caraveo"
+    },
+    {
+        "email": "Carlos@example.com",
+        "password": "12345678",
+        "name": "Carlos Carballo"
+    }
+]
